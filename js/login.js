dfbe6b7c1bb9cd67ba9fa08f7623e9f2ac25d7a5
muriloorias <mu.a.o@outlook.com>
2026-02-24 15:40:01 -0300

    adding login and pages

--- js/login.js
+++ js/login.js
@@ -1,7 +1,19 @@
 function MakeLogin(){
+      var nameInput = document.getElementById('name') 
       var emailinput = document.getElementById('email') 
       var passwordInput = document.getElementById('password')
+      var name = nameInput.value
       var email = emailinput.value
       var password = passwordInput.value
-      //api put
+      var fakeName = "aleandre de urais"
+      var fakeEmail = "email@legal.com"
+      var fakePassword = "1234faahhhh"
+
+      if(name === fakeName && email === fakeEmail &&
+             password === fakePassword){
+            alert("login concluido")
+            window.location.href = '../pages/home.html'
+      } else{
+            alert("tente novamente")
+      }
 }
