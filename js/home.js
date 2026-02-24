f9b22f156de73fcce3526ce50382012b12366257
muriloorias <156871325+muriloorias@users.noreply.github.com>
2026-02-24 12:30:52 -0300

    adding pages

---
+++ js/home.js
@@ -0,0 +1,16 @@
+function goTologin(){
+      window.location.href = '../pages/login.html'
+}
+
+function goToPayments(){
+      window.location.href = '../pages/payments.html'
+}
+
+function goToServers(){
+      window.location.href = '../pages/publicServers.html'
+}
+
+function goToAid(){
+      window.location.href = '../pages/aid.html'
+}
+
---
+++ js/login.js
@@ -0,0 +1,7 @@
+function MakeLogin(){
+      var emailinput = document.getElementById('email') 
+      var passwordInput = document.getElementById('password')
+      var email = emailinput.value
+      var password = passwordInput.value
+      //api put
+}
